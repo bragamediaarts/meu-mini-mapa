b3b602b4a789c106f619e3873a9f8ee1062c5b22
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2018-11-30 11:13:58 +0000

    Test creating another map

---
+++ _Mapa-Exemplo2/config.js
@@ -0,0 +1,41 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Secundária Alberto Sampaio",
+	titulo: "Alberto Sampaio",
+	ano: "9",
+	turma: "B2",
+	creditos:
+		`
+<p>
+De que é feito o património da nossa cidade? Uma cidade são as suas pessoas, os seus edifícios,
+mas também os seus sons e as suas expressões. O meu mini mapa sonoro convida uma turma, um grupo
+ou uma comunidade a fazer a sua própria viagem pelo património material e imaterial de Braga.
+Partindo de seis locais emblemáticos, a aventura passa por criar uma ilustração do seu próprio mapa e pela
+gravação dos sons que o rodeiam e caracterizam. Esta experiência ficará disponível online em forma de mapa
+interactivo visual e sonoro. Assim todos podem desfrutar dos sons de Braga.
+</p>
+ <p>
+	Projecto Serviço Educativo Braga Media Artes<br>
+	Formadores: Marta Pombeiro & Artur Carvalho
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`,
+	sons: [
+		"sons/1_CPEstacao_Carris_master.mp3",
+		"sons/2_CPEstacao_Partida_master.mp3",
+		"sons/3_ElevadorSemArranque_master.mp3",
+		"sons/4_MercadoGalos_master.mp3",
+		"sons/5_MercadoPercurso_master.mp3",
+		"sons/6_MiradouroBaloicoseVento_master.mp3",
+		"sons/7_MissaLavaPes_master.mp3",
+		"sons/8_SemanaSanta_BandaeMatracas_master.mp3",
+		"sons/9_SemanaSanta_BandaFim_master.mp3",
+		"sons/10_SemanaSanta_Estacas_master.mp3"
+	]
+}
