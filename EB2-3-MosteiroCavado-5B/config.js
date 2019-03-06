397e664419bb6501c0ae3acc8b9a6106f4986c5d
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2019-03-06 21:47:48 +0000

    Add files via upload

---
+++ EB2-3-MosteiroCavado-5B/config.js
@@ -0,0 +1,45 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Básica 2,3 Mosteiro e Cávado",
+	titulo: "EB 2,3 Mosteiro e Cávado",
+	ano: "5",
+	turma: "B",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Alexandre; Beatriz; Beatriz; César; Clara; Diana; Diogo; Gabriela; Gonçalo S.; Guilherme; Guilherme C.; Inês; Íris; João; Margarida; Maria; Nara; Rafaela; Tomás C.; Tomás S.
+</p>
+<p>
+De que é feito o património da nossa cidade? Uma cidade são as suas pessoas, os seus edifícios,
+mas também os seus sons e as suas expressões. O meu mini mapa sonoro convida uma turma, um grupo
+ou uma comunidade a fazer a sua própria viagem pelo património material e imaterial de Braga.
+Partindo de seis locais emblemáticos, a aventura passa por criar uma ilustração do seu próprio mapa e pela
+gravação dos sons que o rodeiam e caracterizam. Esta experiência ficará disponível online em forma de mapa
+interactivo visual e sonoro. Assim todos podem desfrutar dos sons de Braga.
+</p>
+ <p>
+	Projecto Serviço Educativo Braga Media Artes<br>
+	Formadores: Marta Pombeiro & Artur Carvalho
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_Recreio.mp3",
+		"sons/2_Torneira.mp3",
+		"sons/3_Cartao.mp3",
+		"sons/4_Porta.mp3",
+		"sons/5_Bar.mp3",
+		"sons/6_Passaros.mp3",
+		"sons/7_Serra.mp3",
+		"sons/8_Pavilhao.mp3",
+		"sons/9_Relogio.mp3",
+		"sons/10_Flauta.mp3"
+	]
+}
