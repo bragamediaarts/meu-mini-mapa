26d9a1092cbf5f8716f3ab3b9a781c5133aa279f
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2019-04-26 13:06:49 +0100

    Add files via upload

---
+++ EB2-3-Gualtar-5G/config.js
@@ -0,0 +1,45 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Básica 2,3 Gualtar",
+	titulo: "EB 2,3 Gualtar",
+	ano: "5",
+	turma: "G",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Ana Rita; Célia; Cláudia; Cristiana; Diogo ; Eduardo; Filipa; Gonçalo; Iara; João Fabiano; João Luis ; João Pedro; Laura; Leonor; Raquel; Renato ; Rodrigo; Sofia; Sónia ; Tiago e  Tiago.
+</p>
+<p>
+De que é feito o património da nossa cidade? Uma cidade são as suas pessoas, os seus edifícios,
+mas também os seus sons e as suas expressões. O meu mini mapa sonoro convida uma turma, um grupo
+ou uma comunidade a fazer a sua própria viagem pelo património material e imaterial de Braga.
+Partindo de seis locais emblemáticos, a aventura passa por criar uma ilustração do seu próprio mapa e pela
+gravação dos sons que o rodeiam e caracterizam. Esta experiência ficará disponível online em forma de mapa
+interactivo visual e sonoro. Assim todos podem desfrutar dos sons de Braga.
+</p>
+ <p>
+	Projecto Serviço Educativo Braga Media Artes<br>
+	Formadores: Marta Pombeiro & Artur Carvalho
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_CampoFutebol.mp3",
+		"sons/2_Cacifos.mp3",
+		"sons/3_Bebedouro.mp3",
+		"sons/4_Tacho.mp3",
+		"sons/5_Pavilhao.mp3",
+		"sons/6_Primaria.mp3",
+		"sons/7_Autoclismo.mp3",
+		"sons/8_Bar.mp3",
+		"sons/9_Escadas.mp3",
+		"sons/10_Aluno.mp3"
+	]
+}
