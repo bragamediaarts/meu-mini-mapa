ae5c498e755ded12f686013ea56e7e99f429c481
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2019-10-30 14:51:31 +0000

    Add files via upload

---
+++ EB-2-3-Palmeira-6-C/config.js
@@ -0,0 +1,45 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Básica 2,3 Palmeira",
+	titulo: "EB 2,3 Palmeira",
+	ano: "6",
+	turma: "C",
+	creditos: `
+<b>Captações, Fotografias e Ilustrações:</b>
+<p>
+	Turma do 6º C.
+</p>
+<p>
+De que é feito o património da nossa cidade? Uma cidade são as suas pessoas, os seus edifícios,
+mas também os seus sons e as suas expressões. O meu mini mapa sonoro convida uma turma, um grupo
+ou uma comunidade a fazer a sua própria viagem pelo património material e imaterial de Braga.
+Partindo de dez locais emblemáticos, a aventura passa por criar uma ilustração do seu próprio mapa e pela
+gravação dos sons que o rodeiam e caracterizam. Esta experiência ficará disponível online em forma de mapa
+interactivo visual e sonoro. Assim todos podem desfrutar dos sons de Braga.
+</p>
+ <p>
+	Projecto Serviço Educativo Braga Media Artes<br>
+	Formadores: Marta Pombeiro & Artur Carvalho
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_Senorita.mp3",
+		"sons/2_Pessoa.mp3",
+		"sons/3_Sophia.mp3",
+		"sons/4_Hino.mp3",
+		"sons/5_Vozes.mp3",
+		"sons/6_Livros.mp3",
+		"sons/7_Palmas.mp3",
+		"sons/8_Recreio.mp3",
+		"sons/9_Passos.mp3",
+		"sons/10_Cartao.mp3"
+	]
+}
