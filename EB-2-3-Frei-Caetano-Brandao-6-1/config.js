b34fec93e5a90fa7ab0e0de490798ac9548ce09b
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2019-12-01 22:51:37 +0000

    Add files via upload

---
+++ EB-2-3-Frei-Caetano-Brandao-6-1/config.js
@@ -0,0 +1,46 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Básica 2,3 Frei Caetano Brandão",
+	titulo: "EB 2,3 Frei Caetano Brandão",
+	ano: "6",
+	turma: "1",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 6º 1.
+<p>Agradecimentos: Rádio Escolar EB 2,3 Frei Caetano Brandão
+</p>
+<p>
+De que é feito o património da nossa cidade? Uma cidade são as suas pessoas, os seus edifícios,
+mas também os seus sons e as suas expressões. O meu mini mapa sonoro convida uma turma, um grupo
+ou uma comunidade a fazer a sua própria viagem pelo património material e imaterial de Braga.
+Partindo de dez locais emblemáticos, a aventura passa por criar uma ilustração do seu próprio mapa e pela
+gravação dos sons que o rodeiam e caracterizam. Esta experiência ficará disponível online em forma de mapa
+interactivo visual e sonoro. Assim todos podem desfrutar dos sons de Braga.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Artur Carvalho & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_agua.mp3",
+		"",
+		"sons/3_radio.mp3",
+		"",
+		"sons/5_portao.mp3",
+		"sons/6_lavrar.mp3",
+		"sons/7_chapeus.mp3",
+		"sons/8_lixo.mp3",
+		"sons/9_fotocopiadora.mp3",
+		"sons/10_cesto.mp3"
+	]
+}
