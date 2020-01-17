7b72eced9e61989bb95d21d835b4c5e75dbbd6da
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-01-17 11:00:50 +0000

    Add files via upload

---
+++ ES-Carlos-Amarante-9B1/config.js
@@ -0,0 +1,44 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Secundária Carlos Amarante",
+	titulo: "Escola Secundária Carlos Amarante",
+	ano: "9",
+	turma: "B1",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 9º B1.
+<p>
+De que é feito o património da nossa cidade? Uma cidade são as suas pessoas, os seus edifícios,
+mas também os seus sons e as suas expressões. O meu mini mapa sonoro convida uma turma, um grupo
+ou uma comunidade a fazer a sua própria viagem pelo património material e imaterial de Braga.
+Partindo de dez locais emblemáticos, a aventura passa por criar uma ilustração do seu próprio mapa e pela
+gravação dos sons que o rodeiam e caracterizam. Esta experiência ficará disponível online em forma de mapa
+interactivo visual e sonoro. Assim todos podem desfrutar dos sons de Braga.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Artur Carvalho & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_oficinas.mp3",
+		"sons/2_pavilhao.mp3",
+		"sons/3_bar.mp3",
+		"sons/4_matraquilhos.mp3",
+		"sons/5_maquina.mp3",
+		"sons/6_radio.mp3",
+		"sons/7_bicicleta.mp3",
+		"sons/8_ginasio.mp3",
+		"sons/9_pingpong.mp3",
+		"sons/10_piano.mp3"
+	]
+}
