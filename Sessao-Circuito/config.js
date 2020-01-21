f4993d2562bdf1e31f7babaf82a862b9d82e2cbe
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-01-21 12:47:36 +0000

    Add files via upload

---
+++ Sessao-Circuito/config.js
@@ -0,0 +1,44 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Conferência Circuito Programação Janeiro-Julho 2020",
+	titulo: "Conferência Circuito",
+	ano: "2020",
+	turma: "Município de Braga",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Wonder Team Município de Braga.
+<p>
+De que é feito o património da nossa cidade? Uma cidade são as suas pessoas, os seus edifícios,
+mas também os seus sons e as suas expressões. O meu mini mapa sonoro convida uma turma, um grupo
+ou uma comunidade a fazer a sua própria viagem pelo património material e imaterial de Braga.
+Partindo de dez locais emblemáticos, a aventura passa por criar uma ilustração do seu próprio mapa e pela
+gravação dos sons que o rodeiam e caracterizam. Esta experiência ficará disponível online em forma de mapa
+interativo visual e sonoro. Assim todos podem desfrutar dos sons de Braga.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Artur Carvalho & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_rececao.mp3",
+		"sons/2_tag.mp3",
+		"sons/3_corredor.mp3",
+		"sons/4_passos.mp3",
+		"sons/5_multiusos.mp3",
+		"",
+		"sons/7_telefone.mp3",
+		"sons/8_patio.mp3",
+		"sons/9_maquina.mp3",
+		"sons/10_mavy.mp3"
+	]
+}
