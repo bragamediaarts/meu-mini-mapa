5473ca43f5d92cdea7681655b56d629ff74e22e1
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-02-20 11:49:25 +0000

    Add files via upload

---
+++ EB-2-3-Cabreiros-8B/config.js
@@ -0,0 +1,44 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Básica 2,3 de Cabreiros",
+	titulo: "Escola Básica 2,3 de Cabreiros",
+	ano: "8",
+	turma: "B",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 8º B.
+<p>
+De que é feito o património da nossa cidade? Uma cidade são as suas pessoas, os seus edifícios,
+mas também os seus sons e as suas expressões. O meu mini mapa sonoro convida uma turma, um grupo
+ou uma comunidade a fazer a sua própria viagem pelo património material e imaterial de Braga.
+Partindo de dez locais emblemáticos, a aventura passa por criar uma ilustração do seu próprio mapa e pela
+gravação dos sons que o rodeiam e caracterizam. Esta experiência ficará disponível online em forma de mapa
+interactivo visual e sonoro. Assim todos podem desfrutar dos sons de Braga.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Artur Carvalho & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_cadeira.mp3",
+		"sons/2_estojos.mp3",
+		"sons/3_estores.mp3",
+		"sons/4_botao.mp3",
+		"sons/5_radiador.mp3",
+		"sons/6_poste.mp3",
+		"sons/7_escadas.mp3",
+		"sons/8_cartao.mp3",
+		"sons/9_estufa.mp3",
+		"sons/10_cacifos.mp3"
+	]
+}
