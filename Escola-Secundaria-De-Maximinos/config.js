efd7720b1800e5fb22fd51b2501dcf16c6b99d9d
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-06-11 19:58:23 +0100

    Update config.js

--- Escola-Secundaria-De-Maximinos/config.js
+++ Escola-Secundaria-De-Maximinos/config.js
@@ -1,7 +1,7 @@
 const MeuMiniMapaConfig = {
 	mapa: "mapa.jpg",
 	nome: "Escola Secundária de Maximinos",
-	titulo: "Grupo de alunos cegos e com baixa visão da Escola Sexundária de Maximinos",
+	titulo: "Grupo de alunos cegos e com baixa visão",
 	ano: "2020",
 	turma: "Junho",
 	creditos: `
@@ -11,7 +11,7 @@ const MeuMiniMapaConfig = {
 <p>
 Este é Mini Mapa Sonoro à distância.
 Durante o período de confinamento o Mini Mapa Sonoro acompanhou os alunos das escolas do concelho de Braga numa viagem pelo património sonoro das suas casas. Virtualmente, fomos ao encontro de professores e estudantes para que recolhessem os sons presentes nas suas casas e ilustrassem depois o que ouviam. O resultado é um mapa interativo visual e sonoro, diferente do da escola, mas igualmente cheio de surpresas e escutas inesperadas.
-Nesta página vais encontrar seis botões disfarçados com fotografias por cima. Terás três em cima (um do lado esquerdo, um no centro e outro no lado direito). Os outros três encontram-se na mesma posição mas mais a baixo. Clica sobre eles tenta descobrir o que cada pessoa gravou.
+Nesta página vais encontrar seis botões disfarçados com fotografias por cima. Terás três em cima (um do lado esquerdo, um no centro e outro no lado direito). Os outros três encontram-se na mesma posição mas mais a baixo. Clica sobre eles e tenta descobrir o que cada pessoa gravou.
 </p>
  <p>
 	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
