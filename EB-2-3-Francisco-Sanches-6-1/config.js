14f6d2a764d26b1d85bcf0d8ef0bf99caa70bede
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-06-13 17:45:28 +0100

    Add files via upload

---
+++ EB-2-3-Francisco-Sanches-6-1/config.js
@@ -0,0 +1,40 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Básica 2,3 Dr. Francisco Sanches",
+	titulo: "Escola Básica 2,3 Dr. Francisco Sanches",
+	ano: "6",
+	turma: "1",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 6º 1.
+<p>
+Este é o Mini Mapa Sonoro à distância.
+Durante o período de confinamento o Mini Mapa Sonoro acompanhou os alunos dos 2º e 3º ciclos das escolas do concelho de Braga numa viagem pelo património sonoro das suas casas. Virtualmente, fomos ao encontro de professores e estudantes para que recolhessem os sons presentes nas suas casas e ilustrassem depois o que ouviam. O resultado é um mapa interativo visual e sonoro, diferente do da escola, mas igualmente cheio de surpresas e escutas inesperadas.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Artur Carvalho & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_porta.mp3",
+		"sons/2_campainha.mp3",
+		"sons/3_leitura.mp3",
+		"sons/4_pintura.mp3",
+		"sons/5_aspirador.mp3",
+		"sons/6_bebe.mp3",
+		"sons/7_despertador.mp3",
+		"sons/8_rua.mp3",
+		"sons/9_persiana.mp3",
+		""
+	]
+}
