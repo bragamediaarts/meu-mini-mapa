9c597cd786ff02d1e3ad9a2efe2f51ee4408ae53
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-06-22 17:56:29 +0100

    Add files via upload

---
+++ Equipa-CERCI-Exterior/config.js
@@ -0,0 +1,39 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "CERCI Braga",
+	titulo: "CERCI Braga",
+	creditos: `
+<b>Captações e Fotografias:</b>
+<p>
+	CERCI Braga.
+<p>
+Este é Mini Mapa Sonoro à distância.
+Durante o período de confinamento o Mini Mapa Sonoro acompanhou os alunos das escolas do concelho de Braga e outros grupos dentro da comunidade, numa viagem pelo património sonoro das suas casas. Virtualmente, fomos ao seu encontro para que recolhessem os sons presentes nos seus lares e ilustrassem depois o que ouviam. O resultado é um mapa interativo visual e sonoro, diferente do da escola e dos mapas tradicionais, mas igualmente cheio de surpresas e escutas inesperadas.
+Explora esta página observando o mapa e clica nas manchas de cores. Irás descobrir novos ambientes.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Artur Carvalho & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_portao.mp3",
+		"",
+		"sons/3_carrinha.mp3",
+		"",
+		"sons/5_varrer.mp3",
+		"",
+		"",
+		"sons/8_max.mp3",
+		"",
+		""
+	]
+}
