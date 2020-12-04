52ad189f9bde5b2b5f3a76221fb200a7873c7eea
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-12-04 10:26:41 +0000

    Add files via upload

---
+++ ES_Dona_Maria_II_11J/config.js
@@ -0,0 +1,40 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Secundária Dona Maria II",
+	titulo: "Escola Secundária Dona Maria II",
+	ano: "11",
+	turma: "J",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 11ºJ.
+<p>
+Este é o Mini Mapa Sonoro à distância.
+Durante o período de confinamento o Mini Mapa Sonoro acompanhou os alunos dos 2º e 3º ciclos das escolas do concelho de Braga numa viagem pelo património sonoro das suas casas. Virtualmente, fomos ao encontro de professores e estudantes para que recolhessem os sons presentes nas suas casas e ilustrassem depois o que ouviam. O resultado é um mapa interativo visual e sonoro, diferente do da escola, mas igualmente cheio de surpresas e escutas inesperadas.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Inês Malheiro & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"",
+		"",
+		"",
+		"",
+		"",
+		"sons/C_melodias.mp3",
+		"sons/D_prof.mp3",
+		"sons/A_sala.mp3",
+		"sons/F_riso.mp3",
+		""
+	]
+}
