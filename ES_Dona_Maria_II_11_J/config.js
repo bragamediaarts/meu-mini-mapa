765411893f68879b41aa2d86d8af114191572f6a
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-12-14 11:52:54 +0000

    Add files via upload

---
+++ ES_Dona_Maria_II_11_J/config.js
@@ -0,0 +1,40 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Secundária Dona Maria II",
+	titulo: "Escola Secundária Dona Maria II",
+	ano: "11",
+	turma: "J",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 11ºJ.
+<p>
+Este Mini Mapa Sonoro é resultado do trabalho colaborativo do 11ºJ, feito com o objectivo de dar a conhecer as suas experiências, perspectivas e memórias da cidade.
+Neste mapa encontrarás sons e imagens, mas também sensações, cheiros, cores e sabores. Assim propomos-te uma viagem diferente por Braga.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Inês Malheiro & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_passaros",
+		""
+		"sons/3_rua",
+		"",
+		"sons/5_dietas",
+		"sons/6_melodias.mp3",
+		"sons/7_prof.mp3",
+		"sons/8_sala.mp3",
+		"",
+		"sons/10_riso"
+	]
+}
