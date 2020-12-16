62408438a591df361a5a1157818caed0618654b0
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-12-16 19:27:52 +0000

    Add files via upload

---
+++ CERCI_Braga/config.js
@@ -0,0 +1,40 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "CERCI Braga",
+	titulo: "CERCI Braga",
+	ano: "2020",
+	turma: "CERCI Braga",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Grupo da CERCI Braga.
+<p>
+Este Mini Mapa Sonoro é resultado do trabalho colaborativo feito com um grupo de utentes da CERCI Braga. Numa manhã de Dezembro fomos conhecer as suas rotinas e atividades lúdicas. Primeiro, medimos a temperatura. Depois cantámos, dançámos e ouvimos passar motas imaginárias. Conversámos sobre gelados, fizemos uma pausa para ir à casa de banho e para atender um telefonema importante e quando a campainha tocou, corremos para ir almoçar!
+Neste mapa encontrarás sons, imagens, sensações e memórias. Assim propomos-te uma visita guiada alternativa, feita por quem habita a cidade.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Inês Malheiro & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_termometroC.mp3",
+		"sons/2_campainhaC.mp3",
+		"sons/3_sanitaC.mp3",
+		"sons/4_miguelC.mp3",
+		"sons/5_cantoresC.mp3",
+		"sons/6_telefoneC.mp3",
+		"sons/7_motasC.mp3",
+		"sons/8_bandaC.mp3",
+		"",
+		"sons/10_djdancerC.mp3"
+	]
+}
