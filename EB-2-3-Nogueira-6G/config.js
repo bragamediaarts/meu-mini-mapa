cc12e12a26960067e6c915278decb7dd141ba7fd
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2020-12-18 15:28:07 +0000

    Add files via upload

---
+++ EB-2-3-Nogueira-6G/config.js
@@ -0,0 +1,40 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Básica 2,3 de Nogueira",
+	titulo: "Escola Básica 2,3 de Nogueira",
+	ano: "6",
+	turma: "G",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 6ºG.
+<p>
+Este Mini Mapa Sonoro é resultado do trabalho colaborativo do 6ºG, feito com o objetivo de dar a conhecer as suas experiências, perspetivas e memórias da cidade.
+Neste mapa encontrarás sons e imagens, mas também sensações, cheiros, cores e sabores. Assim propomos-te que viajes por Braga como se fosses um ou uma personagem de Banda Desenhada! Diverte-te!
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Inês Malheiro & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_cao.mp3",
+		"sons/2_cubo.mp3",
+		"sons/3_cafe.mp3",
+		"",
+		"sons/5_varrer.mp3",
+		"sons/6_passaro.mp3",
+		"",
+		"sons/8_autocarro.mp3",
+		"sons/9_nintendo.mp3",
+		"sons/10_matrecos.mp3"
+	]
+}
