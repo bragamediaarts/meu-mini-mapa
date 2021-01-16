c3ed5e118a20e5a86b04d2c2c5fda8d997e49666
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2021-01-16 17:43:50 +0000

    Add files via upload

---
+++ Fundacao_BOMFIM/config.js
@@ -0,0 +1,41 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Fundação Bomfim",
+	titulo: "Fundação Bomfim",
+	ano: "2021",
+	turma: "Centro de Dia Fundação Bomfim",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Grupo do Centro de Dia da Fundação Bomfim.
+<p>
+Este Mini Mapa Sonoro é resultado do trabalho colaborativo feito com um grupo de utentes do Centro de Dia da Fundação Bomfim. No início do ano fomos conhecer as suas rotinas e atividades lúdicas. Ouvimos canções e rimas antigas, conversámos sobre o que faziam antes da pandemia chegar. Tivemos a honra de ouvir o Sr. António a tocar bateria com pratos e talheres. Fingimos que jogávamos ao dominó. Escutámos as crianças a brincar no recreio, fizemos zapping na televisão e à saída, gravámos o trãnsito na estrada.
+<p>
+Neste mapa encontrarás sons, imagens, sensações e memórias. Assim propomos-te uma visita guiada alternativa, feita por quem habita a cidade.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Inês Malheiro & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_bateria.mp3",
+		"",
+		"sons/3_transito.mp3",
+		"",
+		"sons/5_recreio.mp3",
+		"sons/6_domino.mp3",
+		"",
+		"sons/8_tv.mp3",
+		"",
+		"sons/10_luisa.mp3"
+	]
+}
