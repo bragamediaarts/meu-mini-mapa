2080e2b3c7d294281fee51ea666b20a9f70f3b0b
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2021-01-29 20:50:10 +0000

    Add files via upload

---
+++ ANEIS_Minimus/config.js
@@ -0,0 +1,40 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "ANEIS Braga",
+	titulo: "ANEIS Braga",
+	ano: "2021",
+	turma: "Minimus",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Grupo dos Minimus A e Minimus B.
+<p>
+Este Mini Mapa Sonoro é resultado do trabalho colaborativo feito à distância com as crianças e jovens da ANEIS - Associação Nacional para o Estudo e Intervenção na Sobredotação - delegação de Braga, enquadrado no programa PEDAIS.
+Neste mapa encontrarás um pouco do nosso património, em forma de sons, imagens, sensações e memórias. Clica sobre as imagens e deixa-te guiar nesta visita alternativa feita por quem habita a cidade.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Inês Malheiro & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_bebes.mp3",
+		"sons/2_ps.mp3",
+		"sons/3_familia.mp3",
+		"sons/4_trompete.mp3",
+		"sons/5_porquinhos.mp3",
+		"sons/6_varios.mp3",
+		"sons/7_tanque.mp3",
+		"sons/8_memes.mp3",
+		"sons/9_arvore.mp3",
+		"sons/10_gatinhos.mp3"
+	]
+}
