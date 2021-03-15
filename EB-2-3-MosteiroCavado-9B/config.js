adc325c230d1f9445f6177732dec1fa65ced07f2
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2021-03-15 21:16:26 +0000

    Add files via upload

---
+++ EB-2-3-MosteiroCavado-9B/config.js
@@ -0,0 +1,40 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Básica 2,3 de Mosteiro e Cávado",
+	titulo: "Escola Básica 2,3 de Mosteiro e Cávado",
+	ano: "9",
+	turma: "B",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 9ºB.
+<p>
+Este Mini Mapa Sonoro é resultado do trabalho colaborativo do 9ºB, feito em formato online e com o objetivo de dar a conhecer as suas experiências, perspetivas e memórias da cidade, agora em contexto pandémico.
+Neste mapa encontrarás sons e imagens, mas também sensações sobre o uso dos media durante a pandemia. Assim propomos-te uma viagem diferente por Braga e pelas tecnologias que tanto nos trouxeram de bom, ao diminuir a distância entre quem gostamos, como também nos provocaram ansiedade, dependência, saturação e outras dificuldades. Para navegares neste mapa, clica nas legendas e deixa-te guiar pelo mapa sonoro da Pandemedia.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Inês Malheiro & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_aulas.mp3",
+		"",
+		"sons/3_casa.mp3",
+		"sons/4_amigos.mp3",
+		"sons/5_musica.mp3",
+		"sons/6_trabalhos.mp3",
+		"",
+		"sons/8_noticias.mp3",
+		"",
+		"sons/10_netflix.mp3"
+	]
+}
