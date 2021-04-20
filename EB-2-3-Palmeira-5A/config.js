e8c8a854fc7d54e2f5e4334d1950c0dcd56f2c3b
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2021-04-20 17:02:45 +0100

    Add files via upload

---
+++ EB-2-3-Palmeira-5A/config.js
@@ -0,0 +1,43 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Básica 2,3 de Palmeira",
+	titulo: "Escola Básica 2,3 de Palmeira",
+	ano: "5",
+	turma: "A",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 5ºA.
+<p>
+Este Mini Mapa Sonoro é resultado do trabalho colaborativo do 5ºA, feito com o objetivo de dar a conhecer as suas experiências, perspetivas e memórias da cidade.
+<p>
+Neste mapa encontrarás sons e imagens, mas também sensações, cheiros, cores e sabores. Assim propomos-te uma viagem diferente por Braga.</p>
+<p>
+Liga o som do teu dispositivo e clica sobre os círculos a tracejado para poderes desfrutar desta experiência.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Inês Malheiro & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_secadores.mp3",
+		"",
+		"sons/3_pianos.mp3",
+		"",
+		"sons/5_cozinha.mp3",
+		"sons/6_animais.mp3",
+		"",
+		"",
+		"",
+		"sons/10_tempestade.mp3"
+	]
+}
