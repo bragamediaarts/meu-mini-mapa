6a9fbff1d681141a083a5f230a60def5abb5b756
Braga Media Arts <44967901+bragamediaarts@users.noreply.github.com>
2021-04-20 17:02:03 +0100

    Add files via upload

---
+++ ES-Sa-Miranda-83/config.js
@@ -0,0 +1,43 @@
+const MeuMiniMapaConfig = {
+	mapa: "mapa.jpg",
+	nome: "Escola Secundária Sá de Miranda",
+	titulo: "Escola Secundária Sá de Miranda",
+	ano: "8",
+	turma: "3",
+	creditos: `
+<b>Captações e Ilustrações:</b>
+<p>
+	Turma do 8º3.
+<p>
+Este Mini Mapa Sonoro é resultado do trabalho colaborativo do 8º3, feito com o objetivo de dar a conhecer as suas experiências, perspetivas e memórias da cidade.
+<p>
+Neste mapa encontrarás sons e imagens, mas também sensações, cheiros, cores e sabores. Assim propomos-te uma viagem diferente por Braga.</p>
+<p>
+Liga o som do teu dispositivo e clica sobre os círculos a tracejado, para poderes desfrutar desta experiência.
+</p>
+ <p>
+	Projeto Circuito - Serviço Educativo Braga Media Artes<br>
+	Formadores: Inês Malheiro & Marta Pombeiro
+	Software: Nuno Hespanhol
+</p>
+ <p>
+	<a href="http://www.bragamediaarts.com" target="_blank">www.bragamediaarts.com</a>
+ </p>
+ <p>
+	<img src="../_src/svg/logo-unesco.svg">
+ </p>
+`
+	,
+	sons: [
+		"sons/1_agua.mp3",
+		"sons/2_interruptor.mp3",
+		"sons/3_comboio.mp3",
+		"",
+		"sons/5_rua.mp3",
+		"sons/6_diversao.mp3",
+		"",
+		"sons/8_movimento.mp3",
+		"",
+		"sons/10_material.mp3"
+	]
+}
